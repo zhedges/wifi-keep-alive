d3fd924326d172e9a4316d305df7b2dd0d876a58
Zach Hedges <hedges.zach@gmail.com>
2012-05-11 22:24:28 -0700

    Finally got a working application window... MSDN is *awesome*.

--- wifi-keep-alive.cpp
+++ wifi-keep-alive.cpp
@@ -1,17 +1,85 @@
 #include <iostream>
-#include <windows.h>
+#include <string>
 #include <queue>
-
+#include <windows.h>
 #include "wifi-keep-alive.h"
 
-using std::cout;
-using std::cin;
-using std::endl;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-using std::string;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+	LPCSTR AppTitle = "WiFi KeepAlive";
+	
+	WNDCLASS wc;
+	
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc	= WindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH) COLOR_WINDOWFRAME;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = AppTitle;
+	
+	if (!RegisterClass(&wc))
+		return 0;
+	
+	HWND hwnd = CreateWindow(
+					AppTitle, AppTitle,	
+					WS_OVERLAPPEDWINDOW,
+					
+					CW_USEDEFAULT, CW_USEDEFAULT,
+					640, 480,
+					
+					NULL, NULL,	hInstance, NULL );
+		
+	if (hwnd == NULL)
+	{
+		return 1;
+	}
+	
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	
+	// Run the message loop.
+	
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	
+	return 0;
+}
 
-using std::queue;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+		case WM_DESTROY: {
+			PostQuitMessage(0);
+			return 0;
+		}
+		
+		case WM_PAINT: {
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+			FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+			EndPaint(hwnd, &ps);
+			return 0;
+		}
+		
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	// do we ever get here?
+	return 0;
+}
 
+/*
 int main(int argc, char** argv)
 {
 	
@@ -60,4 +128,4 @@ int main(int argc, char** argv)
 		
 	}
 return 0;
-}
+} */
--- wifi-keep-alive.h
+++ wifi-keep-alive.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_WIFI_KEEP_ALIVE
 #define HEADER_WIFI_KEEP_ALIVE
 
-std::string ver = "0.1";
+const char* ver = "0.1";
 std::string prefix = "[wifi-keep-alive] ";
 
 #endif/*HEADER_WIFI_KEEP_ALIVE*/
